5ab0dbf06950ee36dff1d8f293870fadbaec4499
Cory Perkins <link2cory@gmail.com>
2020-06-22 13:12:44 -0700

    added support for styled-components

---
+++ {{cookiecutter.component_name}}/{{cookiecutter.component_name}}.styles.tsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components'
+
+const Styled{{ cookiecutter.component_name }} = styled.div``
+
+export default Styled{{ cookiecutter.component_name }}
--- {{cookiecutter.component_name}}/{{cookiecutter.component_name}}.tsx
+++ {{cookiecutter.component_name}}/{{cookiecutter.component_name}}.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import { {{ cookiecutter.component_name }}Props } from './interfaces'
-import './{{cookiecutter.component_name}}.{{ cookiecutter.stylesheet }}'
+{% if cookiecutter.stylesheet == 'css' or cookiecutter.stylesheet == 'scss' %}
+  import './{{cookiecutter.component_name}}.{{ cookiecutter.stylesheet }}'
+{% elif cookiecutter.stylesheet == 'styled-components' %}
+  import Styled{{ cookiecutter.component_name }} from './{{cookiecutter.component_name}}.styles'
+{% endif %}
 
 const {{ cookiecutter.component_name }}: React.FC<{{cookiecutter.component_name}}Props> = ({}: {{ cookiecutter.component_name }}Props) => (
-  <div className='{{ cookiecutter.component_name_lower }}-container' />
+  {% if cookiecutter.stylesheet == 'css' or cookiecutter.stylesheet == 'scss' %}
+    <div className='{{ cookiecutter.component_name_lower }}-container' />
+  {% elif cookiecutter.stylesheet == 'styled-components' %}
+    <Styled{{ cookiecutter.component_name }} />
+  {% endif %}
 )
 
 export default {{ cookiecutter.component_name }}
