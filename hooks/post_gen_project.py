848fd1327a4a30c7facd38188675eecaec5327fd
Cory Perkins <link2cory@gmail.com>
2020-06-22 13:12:44 -0700

    added support for styled-components

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,17 +4,22 @@ from cookiecutter.main import cookiecutter
 
 context = {{ cookiecutter }}
 
-def setup_stylesheets(stylesheet: str):
+def setup_stylesheets(stylesheet: str, component_name: str):
     # only keep the appropriate stylesheet file
     if stylesheet == 'css':
-        os.remove('{{ cookiecutter.component_name }}.scss')
-    else:
-        os.remove('{{ cookiecutter.component_name }}.css')
+        os.remove(f"{component_name}.scss")
+        os.remove(f"{component_name}.styles.tsx")
+    elif stylesheet == "scss":
+        os.remove(f"{component_name}.css")
+        os.remove(f"{component_name}.styles.tsx")
+    elif stylesheet == "styled-components":
+        os.remove(f"{component_name}.css")
+        os.remove(f"{component_name}.scss")
 
 def setup_testing(testing: str):
     # only keep the test files if requested
     if testing != 'yes':
         os.remove('{{ cookiecutter.component_name }}.spec.tsx')
 
-setup_stylesheets(context['stylesheet'])
+setup_stylesheets(context['stylesheet'], context['component_name'])
 setup_testing(context['testing'])
--- {{cookiecutter.component_name}}/{{cookiecutter.component_name}}.tsx
+++ {{cookiecutter.component_name}}/{{cookiecutter.component_name}}.tsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import { {{ cookiecutter.component_name }}Props } from './interfaces'
-import './{{cookiecutter.component_name}}.{{ cookiecutter.stylesheet }}'
+{% if cookiecutter.stylesheet == 'css' or cookiecutter.stylesheet == 'scss' %}
+  import './{{cookiecutter.component_name}}.{{ cookiecutter.stylesheet }}'
+{% elif cookiecutter.stylesheet == 'styled-components' %}
+  import './{{cookiecutter.component_name}}.styles.tsx'
+{% endif %}
 
 const {{ cookiecutter.component_name }}: React.FC<{{cookiecutter.component_name}}Props> = ({}: {{ cookiecutter.component_name }}Props) => (
   <div className='{{ cookiecutter.component_name_lower }}-container' />
